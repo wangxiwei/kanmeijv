670074dca5d8f61b8916d9ab274d7e36ca82a1b0
xiweiwang <xiweiwang@Ctrip.com>
2016-02-26 19:35:37 +0800

    add model and admin

--- movielist/admin.py
+++ movielist/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('movie_name','movie_state','movie_update','movie_return','movie_countdown')
+    list_filter = ['movie_update']
+    search_fields = ['movie_name']
+
+admin.site.register(Movie,MovieAdmin)
--- movielist/models.py
+++ movielist/models.py
@@ -1,6 +1,16 @@
+# encoding=utf-8
 from __future__ import unicode_literals
 
 from django.db import models
 
 
+class Movie(models.Model):
+    movie_name = models.CharField(max_length=500)
+    movie_url = models.CharField(max_length=1000)
+    movie_state = models.CharField(max_length=50)
+    movie_update = models.CharField(max_length=50)
+    movie_return = models.CharField(max_length=50)
+    movie_countdown = models.CharField(max_length=50)
 
+    def __unicode__(self):
+        return self.movie_name
