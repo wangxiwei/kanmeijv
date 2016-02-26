ab8bab18dd403f6570d44d031702d57d526177ff
xiweiwang <xiweiwang@Ctrip.com>
2016-02-26 20:48:20 +0800

    add index

--- movielist/views.py
+++ movielist/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+from .models import Movie
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    model = Movie
+    template_name = 'movielist/index.html'
+    context_object_name = 'movie_list'
+
+
