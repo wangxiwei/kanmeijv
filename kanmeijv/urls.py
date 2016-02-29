671959be63acec32c2de1d8dec192dc65487870a
xiweiwang <xiweiwang@Ctrip.com>
2016-02-29 18:56:01 +0800

    add django_wsgi.py 文件

--- kanmeijv/urls.py
+++ kanmeijv/urls.py
@@ -19,4 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^movielist/',include('movielist.urls')),
+    url(r'^index',include('movielist.urls')),
+    url(r'^',include('movielist.urls')),
 ]
